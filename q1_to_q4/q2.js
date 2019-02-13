d90652ee0bd611b182db3358543ae400c85f92ca
Mahesh Inder <mahesh.inder85@gmail.com>
2019-02-13 13:03:40 +0530

    Add files via upload

---
+++ q1_to_q4/q2.js
@@ -0,0 +1,24 @@
+function ispalindrome(string) {
+    if (string) {
+        var str = string.toLowerCase();
+        var len = string.length;
+        var mid = Math.floor(len / 2);
+
+        for (var i = 0; i < mid; i++) {
+            if (str[i] !== str[len - 1 - i])
+
+                return false;
+        }
+        return true;
+    } else return false;
+
+}
+
+function callIsPalindrome() {
+    var string = prompt("Enter the String ");
+    var result = ispalindrome(string);
+    if (result)
+        alert("Yaay, it is a palindrome.");
+    else alert("Sorry. Its not.");
+
+}
---
+++ q1_to_q4/q3.js
@@ -0,0 +1,5 @@
+function areaOfCircle() {
+
+    var radius = prompt("Enter radius");
+    alert("The area is " + Math.pow(radius, 2) * 3.14);
+}
