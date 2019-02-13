9f1e2c8327e6c86b7aa4610792923ad6ca0ba2ea
Mahesh Inder <mahesh.inder85@gmail.com>
2019-02-13 13:04:02 +0530

    Delete q1.js

--- q1.js
+++
@@ -1,8 +0,0 @@
-function simpleInterest() {
-
-    var amt = prompt("Enter Amount");
-    var interestRate = prompt("Enter interest rate");
-    var time = prompt("Enter number of years");
-    var simpleInterest = (amt * interestRate * time) / 100;
-    alert("Simple Interest is " + simpleInterest);
-}
