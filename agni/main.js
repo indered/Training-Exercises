ea547237e2c072750b3e1fe4b88c5029dd97eb62
Mahesh Inder <mahesh.inder85@gmail.com>
2019-03-04 01:25:23 +0530

    Add files via upload

---
+++ agni/main.js
@@ -0,0 +1,21 @@
+// function slideshow() {
+//     var imageArr = [];
+//     var i = 0;
+//     imageArr.push("sources/banner-bg.jpg");
+//     imageArr.push("sources/cover2.jpg");
+//     imageArr.push("sources/cover3.jpg");
+
+//     setInterval(function () {
+//         i++;
+//         if (i == imageArr.length) {
+//             i = 0;
+//         }
+
+//         $(".aol").css("background-image", "url(" + imageArr[i] + ")");
+//         $(".aol").slideshow("slide", {
+//             direction: "left"
+//         });
+//     }, 1000);
+
+
+// }
