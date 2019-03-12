bb1d6f712e909f57e8a0b04dc879361510fe7a31
mahesh inder <mahesh.inder85@gmail.com>
2019-03-12 22:42:08 +0530

    complete

--- digiClock/main.js
+++ digiClock/main.js
@@ -1,35 +1,96 @@
-var clock = document.getElementsByClassName("digi-clock");
+var clock = document.getElementById("digi-clock");
+var wish = document.getElementById("wish");
+var cal = document.getElementById("date");
 
-var timestamp = new Date();
-var s = timestamp.getSeconds();
-var m = timestamp.getMinutes();
-var h = timestamp.getHours();
-var date = timestamp.getDate();
-var day = timestamp.getDay();
-var month = timestamp.getMonth();
-var year = timestamp.getFullYear();
-var period = "AM";
-var wish;
+function showMessage() {
+    timestamp = new Date();
+    var h = timestamp.getHours();
 
-function showClock() {
 
-    if (h >= 0 && h < 12) {
-        wish = 'Good Morning';
+    var message;
+
+
+
+    if (h >= 0 && h < 12)
+
+        message = 'Good Morning';
+    else if (h == 12)
+        message = "Good Noon";
+    else if (h > 12 && h < 17)
+        message = "Good Afternoon";
+    else if (h >= 17 && h < 20)
+        message = 'Good Evening';
+    else if (h >= 20 && h < 24) message = 'Good Night';
+
+    wish.innerHTML = message;
+
+    if (h == 0)
+        setTimeout(showMessage, 43200000);
+    if (h == 12)
+        setTimeout(showMessage, 18000000);
+    if (h == 17)
+        setTimeout(showMessage, 10800000);
+    if (h == 20)
+        setTimeout(showMessage, 14400000);
+
+}
+
+function showTime() {
+    timestamp = new Date();
+    var s = timestamp.getSeconds();
+    var m = timestamp.getMinutes();
+    var h = timestamp.getHours();
+    var period = "AM";
+
+
+    if (h == 0) {
+        h = 12;
     }
-
-    if (h == 12) {
-        wish = 'Good Noon';
-    }
-    if (h > 12 && h < 17) wish = 'Good Afternoon';
-
-    if(h>17 )
-
-    if (h == 0) h = 12;
 
     if (h > 12) {
-        h -= 12;
+        h = h - 12;
         period = "PM";
     }
 
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    time = h + ":" + m + ":" + s + " " + period;
+
+    clock.innerHTML = time;
+
+    setTimeout(showTime, 1000);
 
 }
+
+function showDate() {
+    timestamp = new Date();
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var weekDay = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    var date = timestamp.getDate();
+    date = (date < 10) ? "0" + date : date;
+
+    var day = weekDay[timestamp.getDay() - 1];
+
+    var year = timestamp.getFullYear();
+    var month = months[timestamp.getMonth()];
+    var fulldate = day + ", " + date + " " + month + " " + year;
+    cal.innerHTML = fulldate;
+
+    if (timestamp.getHours() == 0)
+        setTimeout(showDate, 24 * 60 * 60 * 1000);
+}
+
+
+
+function showClock() {
+    var timestamp = new Date();
+    showMessage();
+    showTime();
+    showDate();
+
+}
+
+showClock();
